9800cfe0bb5b2bd7a95567d0619890244a4363d8
Similiukas <tautvaisassimas@gmail.com>
2022-07-02 18:03:07 +0300

    add gradual volume decrease before pausing

--- website/src/hooks/AudioHook.tsx
+++ website/src/hooks/AudioHook.tsx
@@ -22,7 +22,30 @@ export default function useAudio(url: string, volume = 0.4): OwnProps {
 
     const toggleAudio = (toggleLive: boolean) => {
         if (!playing && toggleLive) audio.src = `${url}?_=${Math.random()}`;
-        setPlaying(!playing);
+        // Gradually reducing volume before pausing
+        if (playing) {
+            const duration = 400; let i = 0; // 0.4 second but the actual setInterval time is about +300ms due to settings state and all
+            const initialVolume = audio.volume;
+            const P0 = initialVolume * 0.5; const P1 = 0.02; const P2 = 0.16; const P3 = 0;
+            const intervalID = setInterval(() => {
+                if (audio.volume > 0) {
+                    const t = (100 * i++) / duration;
+                    // Cubic bezier curve: https://en.wikipedia.org/wiki/B%C3%A9zier_curve#Cubic_B%C3%A9zier_curves
+                    // With P0 as initial lowered volume and P3 the final. P3 can be negative to get a bigger dip in volume
+                    // eslint-disable-next-line space-infix-ops
+                    const newValue = (1-t)**3*P0 + 3*(1-t)**2*t*P1 + 3*(1-t)*t**2*P2 + t**3*P3;
+                    audio.volume = newValue > 0 ? newValue : 0;
+                } else {
+                    setPlaying(false);
+                    // When the audio is finally paused (not necessarily on next interval) we reset the audio volume and clear interval
+                    if (audio.paused) {
+                        clearInterval(intervalID);
+                        audio.volume = initialVolume;
+                        if (toggleLive) audio.src = ''; // Deleting mount to Icecast for server to update its listener count
+                    }
+                }
+            }, 100);
+        } else setPlaying(true);
     };
 
     const changeAndPlayURL = (givenAudioUrl: string) => {
